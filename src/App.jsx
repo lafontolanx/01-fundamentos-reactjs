543a91b3ad13f3099a6a33bb13e920caf0a0dab0
Laura Fontolan <laura_fontolan@hotmail.com>
2022-12-08 10:55:22 -0300

    css global config

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Header } from './components/Header'
 import { Post } from './Post'
+
+import './global.css'
 
 export function App() {
 
   return (
     <div>
+      <Header />
+
       <Post 
         author="Laura Fontolan" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, veritatis."
