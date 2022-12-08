534942f0e06a56128c17a8c3e59ea6ee7d04ab0a
Laura Fontolan <laura_fontolan@hotmail.com>
2022-12-08 09:20:29 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Post } from './Post'
+
+export function App() {
+
+  return (
+    <div>
+      <Post 
+        author="Laura Fontolan" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, veritatis."
+      />
+      <Post 
+        author="Pedro Henrique" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, veritatis."
+      />
+    </div>
+  )
+}
+
+
