0bf914b02466073b3f5cab5fa703d4a1590e5d27
Laura Fontolan <laura_fontolan@hotmail.com>
2022-12-13 13:15:16 -0300

    fix post import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from './components/Header'
-import { Post } from './Post'
+import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar'
 
 import styles from './App.module.css'
 
@@ -12,7 +13,7 @@ export function App() {
       <Header />
 
       <div className={styles.wrapper}>
-        <aside></aside>
+        <Sidebar />
         <main>
           <Post 
             author='Laura Fontolan'
