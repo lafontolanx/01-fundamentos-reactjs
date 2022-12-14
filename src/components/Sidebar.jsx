862654aaa02da7243af333548a3b5078cd0f5a41
Laura Fontolan <laura_fontolan@hotmail.com>
2022-12-14 13:19:30 -0300

    fix sidebar style

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css'
 
@@ -11,7 +12,7 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/lafontolanx.png" />
+                <Avatar src="https://github.com/lafontolanx.png"/>
 
                 <strong>Laura Fontolan</strong>
                 <span>Web Developer</span>
