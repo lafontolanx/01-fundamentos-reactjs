7f7be7e0f6b34923c90494315d355907c7d7c591
Laura Fontolan <laura_fontolan@hotmail.com>
2022-12-14 13:19:18 -0300

    fix post style

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,5 @@
+import { Avatar } from './Avatar'
+import { Comment } from './Comment'
 import styles from './Post.module.css'
 
 export function Post() {
@@ -5,7 +7,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img src="https://github.com/lafontolanx.png" />
+                    <Avatar src="https://github.com/lafontolanx.png" />
                     <div className={styles.authorInfo}>
                         <strong>Laura Fontolan</strong>
                         <span>Web Developer</span>
@@ -24,6 +26,24 @@ export function Post() {
                     <a href="">#nlw</a>{' '}
                     <a href="">#rocketseat</a>
                 </p> 
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea 
+                    placeholder="Deixe um comentário"
+                />
+
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>
+            </form>
+
+            <div className={styles.commentList}>
+                <Comment />
+                <Comment />
+                <Comment />
             </div>
         </article>
     )
